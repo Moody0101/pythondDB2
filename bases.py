09df58d3a206918917ca93334226c31e02a7c3bb
Moody0101 <azmoudh@gmail.com>
2021-11-13 14:34:07 +0100

    Fixed issues

--- bases.py
+++ bases.py
@@ -1,14 +1,18 @@
+"""
+
+a Module to encode and decode to base64 and other encodings using the data 
+provided from the command line or using data from another script. 
+
+made it because it is not cool to keep doing unnecessary stuff
+that module wrapps the most useful from base64 module.
+
+"""
+
 
 
 from base64 import *
 from typing import Union
-
-"""
-a wrapper for the base64 library.
-every class implements some base function.
-every method takes in a byte-like object so basically, don't 
-pass a string. or int.
-"""
+from colorama import Fore
 
 
 class base64:
@@ -32,10 +36,18 @@ class base64:
 class base32:
 	def __init__(self, data: Union[bytes, str]):
 		self.data = data
-	def _encode(self, bytes=False):
-		return b32encode(self.data)
+	def _encode(self):
+		if isinstance(self.data, bytes):
+			return b32encode(self.data).decode()
+		else:
+			return b32encode(self.data.encode()).decode()
+
 	def _decode(self):
-		return b32decode(self.data)
+		if isinstance(self.data, bytes):
+			return b32decode(self.data).decode()
+		else:
+			return b32decode(self.data.encode()).decode()
+
 	def __str__(self):
 		return str(self.__class__.__name__)
 
@@ -43,10 +55,18 @@ class base32:
 class base16:
 	def __init__(self, data: Union[bytes, str]):
 		self.data = data
-	def _encode(self, bytes=False):
-		return b16encode(self.data)
+	def _encode(self):
+		if isinstance(self.data, bytes):
+			return b16encode(self.data).decode()
+		else:
+			return b16encode(self.data.encode()).decode()
+
 	def _decode(self):	
-		return b16decode(self.data)
+		if isinstance(self.data, bytes):
+			return b16decode(self.data).decode()
+		else:
+			return b16decode(self.data.encode()).decode()
+
 	def __str__(self):
 		return str(self.__class__.__name__)
 
@@ -55,25 +75,41 @@ class a85:
 
 	def __init__(self, data: Union[bytes, str]):
 		self.data = data.decod
-	def _encode(self, bytes=False):
-		return a85encode(self.data)
-	def _decode(self):	
-		return a85decode(self.data)
+	def _encode(self):
+		if isinstance(self.data, bytes):
+			return a85encode(self.data).decode()
+		else:
+			return a85encode(self.data.encode()).decode()
+
+	def _decode(self):
+		if isinstance(self.data, bytes):
+			return a85decode(self.data).decode()
+		else:
+			return a85decode(self.data.encode()).decode()
+
 	def __str__(self):
 		return str(self.__class__.__name__)
 
 class b85:
 	def __init__(self, data: Union[bytes, str]):
 		self.data = data
-	def _encode(self, bytes=False):
-		return b85encode(self.data)
+	def _encode(self):
+		if isinstance(self.data, bytes):
+			return b85encode(self.data).decode()
+		else:
+			return b85encode(self.data.encode()).decode()
+
 	def _decode(self):	
-		return b85decode(self.data)
+		if isinstance(self.data, bytes):
+			return b85decode(self.data).decode()
+		else:
+			return b85decode(self.data.encode()).decode()
+
 	def __str__(self):
 		return str(self.__class__.__name__)
 # testing
 
 if __name__ == '__main__':
 	for i in [base16, b85, base32, base64]:
-		ins = i(b'hello')
-		print(f"{str(ins)} => ",ins._encode().decode("utf-8"))
+		ins = i('hello')
+		print(f"{Fore.YELLOW}{str(ins)} => {Fore.CYAN}", f"{ins._encode()}{Fore.WHITE}")
